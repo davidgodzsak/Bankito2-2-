2bb56385f89a9f22920ba853d5cf5e7bad9876e3
Dávid Godzsák <dg@shape.dk>
2019-12-06 21:00:18 +0100

    more bubbles

--- script.js
+++ script.js
@@ -5,6 +5,14 @@
  *
  * @author Dávid Godzsák (insta: @godzsaaa, @made.by.david)
  */
+//CONST
+var MIN_BUBBLE_AMOUNT = 5;
+var EXTRA_BUBBLES = 30;
+
+//GLOBAL VARS
+var isScrolling = false;
+var scrollBackCallBack = null;
+var initialScrollEvent = true;
 
 //UTIL
 function classNameForEach(className, fn) {
@@ -13,6 +21,12 @@ function classNameForEach(className, fn) {
     for (var i = 0; i < items.length; i++) {
         fn(items[i])
     }
+}
+
+function runLaterWith(f, i, t) {
+    setTimeout(function () {
+        f(i)
+    }, t);
 }
 
 //INITIALIZATION FN
@@ -30,24 +44,82 @@ function shuffleDrop() {
     });
 }
 
+function makeBubble(bubble, now) {
+    var newBubble = bubble.cloneNode(true);
+    var size = Math.random() * 70 + 10;
+
+    newBubble.style.width = size + "%";
+    newBubble.children[0].style.animationDirection = Math.random() > 0.5 ? "alternate" : "alternate-reverse";
+    newBubble.style.left = Math.random() * 100 + "px";
+    newBubble.style.animationDuration = size * 15 + 1200 + "ms";
+    if (now) {
+        newBubble.style.animationDelay = 0 + "ms";
+    }
+
+    bubble.parentNode.appendChild(newBubble);
+}
+
 function turnOnBubbleBath() {
     classNameForEach("bubbles", function (it) {
         var bubble = it.children[0];
 
-        for (var j = 0; j < Math.random() * 20 + 5; j++) {
-            setTimeout(function () {
-                var newBubble = bubble.cloneNode(true);
-                var size = Math.random() * 70 + 10;
-
-                newBubble.style.width = size + "%";
-                newBubble.children[0].style.animationDirection = Math.random() > 0.5 ? "alternate" : "alternate-reverse";
-                newBubble.style.left = Math.random() * 100 + "px";
-                newBubble.style.animationDuration = size * 15 + 1200 + "ms";
-
-                bubble.parentNode.appendChild(newBubble);
-            }, Math.random() * 4000)
-        }})
+        for (var j = 0; j < Math.random() * 20 + MIN_BUBBLE_AMOUNT; j++) {
+            runLaterWith(makeBubble, bubble, Math.random() * 4000)
+        }
+    })
 }
+
+function addBubbles(bubble, amount) {
+    for (var i = 0; i < amount; i++) {
+        runLaterWith(function () {
+            makeBubble(bubble, true);
+        }, null, Math.random() * 400);
+    }
+}
+
+function removeBubbles(bubble, amount) {
+    var children = bubble.parentNode.children;
+    var realAmount = amount > MIN_BUBBLE_AMOUNT ? amount : 0;
+
+    for (var i = 1; i < realAmount; i++) {
+        runLaterWith(function (a) {
+            a.remove()
+        }, children[i], Math.random() * 800);
+    }
+}
+
+function updateBubbleBath() {
+    classNameForEach("bubbles", function (it) {
+        var bubble = it.children[0];
+        var existingBubbles = it.children.length;
+        var numberOfBubbles = Math.round(Math.random() * 20 + MIN_BUBBLE_AMOUNT + (isScrolling ? EXTRA_BUBBLES : 0));
+
+        let bubbleDelta = existingBubbles - numberOfBubbles;
+        if (bubbleDelta > 0) {
+            removeBubbles(bubble, bubbleDelta);
+        } else {
+            addBubbles(bubble, -bubbleDelta);
+        }
+    })
+}
+
+//LISTENERS
+window.addEventListener('scroll', function (e) {
+    if (initialScrollEvent) {
+        initialScrollEvent = false;
+        return;
+    }
+    if (!isScrolling) {
+        isScrolling = true;
+        updateBubbleBath();
+    }
+
+    clearTimeout(scrollBackCallBack);
+    scrollBackCallBack = setTimeout(function () {
+        isScrolling = false;
+        updateBubbleBath();
+    }, 600);
+});
 
 // MAIN
 shuffleDrop();
